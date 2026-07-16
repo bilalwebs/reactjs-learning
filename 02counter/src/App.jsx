d713256609f20915d319a1c18dc4ea657f529629
bilalwebs <bil2023aljutt@gmail.com>
2026-07-16 10:24:55 +0500

    feat: add React counter app using useState

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [counter, setCounter] = useState(12);
+
+  const addValue = () => {
+    setCounter(counter + 1);
+  };
+
+  const removeCounter = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <>
+      <h1>Bilal Reactjs Learning</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+
+      <br />
+      <br />
+
+      <button onClick={removeCounter}>Remove Value</button>
+    </>
+  );
+}
+
+export default App;
